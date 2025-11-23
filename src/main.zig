e4994282cba27f74e2bd8f8131ce5156d1b08fc7
Tachera Sasi <tacherasasi@gmail.com>
2025-11-23 14:30:47 +0300

    Add usage message for insufficient arguments in main function

--- src/main.zig
+++ src/main.zig
@@ -9,6 +9,11 @@ pub fn main() !void {
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
 
+    if (args.len < 2) {
+        std.debug.print("Usage: {s} <binary_path> [args...]\n", .{args[0]});
+        return;
+    }
+
     const bin_path = args[1];// ignoring the first arg which is the program name
     std.debug.print("Binary Path: {s}\n", .{bin_path});
 
