5a188d9e64c18f1f7c97290c9ca106a4db3372bc
Tachera Sasi <tacherasasi@gmail.com>
2025-11-23 14:40:17 +0300

    Add file existence check in main function

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const heap = std.heap;
+const utils = @import("utils.zig");
 
 pub fn main() !void {
     var gpa = heap.GeneralPurposeAllocator(.{}){};
@@ -17,6 +18,12 @@ pub fn main() !void {
     const bin_path = args[1];// ignoring the first arg which is the program name
     std.debug.print("Binary Path: {s}\n", .{bin_path});
 
+    const exists = try utils.FileExists(allocator, bin_path);
+
+    if (!exists) {
+        std.debug.print("File does not exist: {s}\n", .{bin_path});
+        return;
+    }
     for (args) |arg| {
         std.debug.print("Arg: {s}\n", .{arg});
     }
