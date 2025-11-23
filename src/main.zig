b945567a910871b54c8a95f310fd3ee97fae6874
Tachera Sasi <tacherasasi@gmail.com>
2025-11-23 14:21:06 +0300

    Refactor main function for improved variable usage and argument iteration

--- src/main.zig
+++ src/main.zig
@@ -2,14 +2,14 @@ const std = @import("std");
 const heap = std.heap;
 
 pub fn main() !void {
-    const gpa = heap.GeneralPurposeAllocator(.{}){};
+    var gpa = heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
     defer _ = gpa.deinit();
 
-    var args = try std.process.argsAlloc(allocator);
+    const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
 
-    while (args.next()) |arg| {
+    for (args) |arg| {
         std.debug.print("Arg: {s}\n", .{arg});
     }
 }
