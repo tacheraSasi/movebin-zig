e2fc37e7328dd60162b73796e33e2cce332f53f9
tacheraSasi <tacherasasi@gmail.com>
2026-02-01 22:30:21 +0300

    Refactor askYesNo function formatting and add isNoBackupFlag function for argument checks

--- src/utils.zig
+++ src/utils.zig
@@ -12,9 +12,8 @@ pub fn fileExists(path: []const u8) !bool {
     return found;
 }
 
-
 /// Prompt the user with a yes/no question.
-pub fn askYesNo(console:Console, prompt: []const u8, default_yes: bool) !bool {
+pub fn askYesNo(console: Console, prompt: []const u8, default_yes: bool) !bool {
     while (true) {
         if (default_yes) {
             try console.print("{s} [Y/n]: ", .{prompt});
@@ -42,6 +41,19 @@ pub fn isForceFlagEnabled(args: []const []const u8) bool {
     return false;
 }
 
+/// Check if the no-backup flag (--no-backup) is present in the arguments.
+pub fn isNoBackupFlag(args: []const []const u8) bool {
+    for (args) |arg| {
+        if  (std.mem.eql(u8, arg, "--no-backup")) {
+            return true;
+        }
+    }
+    return false;
+}
+
+/// Backup the existing binary and remove it from the destination path.
+/// `usr/local/bin/somebinary` -> `usr/local/bin/.movebin_backups/somebinary_timestamp`
+/// If backup_dir is null, a default hidden directory next to the destination will be used.
 pub fn backupAndRemoveExistingBin(
     allocator: *std.mem.Allocator,
     src_path: []const u8,
@@ -51,12 +63,12 @@ pub fn backupAndRemoveExistingBin(
     const dest_path = try fs.cwd().realPath(src_path);
     const dest_dir = try fs.path.dirname(allocator, dest_path);
     var backup_directory: []const u8 = "";
-    
-    if (backup_dir)  {
+
+    if (backup_dir) {
         backup_directory = backup_dir.?;
     } else {
-        backup_directory = try fs.path.join(allocator, &.{dest_dir, ".movebin_backups"});
-        
+        backup_directory = try fs.path.join(allocator, &.{ dest_dir, ".movebin_backups" });
+
         if (!try fileExists(backup_directory)) {
             try fs.cwd().createDir(backup_directory, 0o755);
         }
@@ -66,19 +78,20 @@ pub fn backupAndRemoveExistingBin(
     const backup_filename = try fs.path.basename(allocator, dest_path);
     const backup_path = try fs.path.join(
         allocator,
-        &.{backup_directory, std.fmt.allocPrint(allocator, "{s}_{d}", .{backup_filename, timestamp})},
+        &.{ backup_directory, std.fmt.allocPrint(allocator, "{s}_{d}", .{ backup_filename, timestamp }) },
     );
 
     // Copy the existing binary to the backup location
     try fs.cwd().copyFile(dest_path, std.fs.cwd(), backup_path, .{});
 
-    // Optionally implement retention logic here to delete old backups
+    // TODO: i will implement retention logic here to delete old backups
 
     // Delete the existing binary
     try deleteExistingBin(dest_path);
 
     return backup_path;
 }
+
 ///Copy the bin to the destination path
 pub fn copyToDestination(src_path: []const u8, dest_path: []const u8) !void {
     try fs.cwd().copyFile(src_path, std.fs.cwd(), dest_path, .{});
