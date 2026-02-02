68b81b4866eef47567d33d837ac3d55c39115eb0
tacheraSasi <tacherasasi@gmail.com>
2026-02-02 11:47:19 +0300

    Update cli-flags.zig

--- src/cli-flags.zig
+++ src/cli-flags.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 /// Initializes the command-line flags.
-const CliFlags = struct {
+pub const CliFlags = struct {
     args: []const u8,
     allocator: std.mem.Allocator,
 
@@ -9,22 +9,22 @@ const CliFlags = struct {
     
     /// Initializes the command-line flags.
     pub fn init(allocator: std.mem.Allocator) !CliFlags {
-        Self.args = try std.process.argsAlloc(allocator);
+        Self.getArgs = try std.process.argsAlloc(allocator);
     }
     
     /// Returns the command-line arguments.
     /// From the first argument (excluding the program name) args[1..].
-    pub fn args(self: Self) []const u8 {
-        return self.args[1..];
+    pub fn getArgs(self: Self) []const u8 {
+        return self.getArgs[1..];
     }
     
     /// Returns the number of command-line arguments.
     pub fn len(self: Self) usize {
-        return self.args.len;
+        return self.getArgs.len;
     }
     
     pub fn isVersionFlagEnabled(self: Self) bool {
-        for (self.args) |arg| {
+        for (self.getArgs) |arg| {
             if (std.mem.eql(u8, arg, "-v") or std.mem.eql(u8, arg, "--version")) {
                 return true;
             }
@@ -34,7 +34,7 @@ const CliFlags = struct {
     
     /// Check if the force flag (-f or --force) is present in the arguments.
     pub fn isForceFlagEnabled(self: Self) bool {
-        for (self.args) |arg| {
+        for (self.getArgs) |arg| {
             if (std.mem.eql(u8, arg, "-f") or std.mem.eql(u8, arg, "--force")) {
                 return true;
             }
@@ -44,7 +44,7 @@ const CliFlags = struct {
     
     /// Check if the no-backup flag (--no-backup) is present in the arguments.
     pub fn isNoBackupFlag(self: Self) bool {
-        for (self.args) |arg| {
+        for (self.getArgs) |arg| {
             if (std.mem.eql(u8, arg, "--no-backup")) {
                 return true;
             }
@@ -54,6 +54,6 @@ const CliFlags = struct {
     
     /// Frees the memory allocated for the command-line arguments.
     pub fn argsFree(self: *Self) void {
-        std.process.argsFree(self.allocator, self.args);
+        std.process.argsFree(self.allocator, self.getArgs);
     }
-}
+};
