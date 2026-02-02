2c2e36a79aa47ba883627800870ddc1f44ddac66
tacheraSasi <tacherasasi@gmail.com>
2026-02-02 11:38:49 +0300

    Update cli-flags.zig

--- src/cli-flags.zig
+++ src/cli-flags.zig
@@ -1,10 +1,19 @@
 const std = @import("std");
 
+/// Initializes the command-line flags.
 const CliFlags = struct {
+    args: []const u8,
+    allocator: std.mem.Allocator,
+
+    const Self = @This();
+    
+    /// Initializes the command-line flags.
     pub fn init(allocator: std.mem.Allocator) !CliFlags {
+        Self.args = try std.process.argsAlloc(allocator);
     }
     
-    pub fn free(){
-        
+    /// Frees the memory allocated for the command-line arguments.
+    pub fn argsFree(self: *Self) {
+        std.process.argsFree(self.allocator, self.args);
     }
 }
