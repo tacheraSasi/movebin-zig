b9f3be2770847ea621dcc3e80df9abec6e9d31af
tacheraSasi <tacherasasi@gmail.com>
2026-07-13 12:43:04 +0300

    Simplify build configuration by removing separate module layer

--- build.zig
+++ build.zig
@@ -4,25 +4,12 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const mod = b.addModule("movebin", .{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-    });
-    
-
     const exe = b.addExecutable(.{
         .name = "movebin",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-            .imports = &.{
-                .{ .name = "movebin", .module = mod },
-            },
-        }),
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
     });
-
-    b.installArtifact(exe);
 
     const stdio_dep = b.dependency("stdio", .{
         .target = target,
@@ -30,21 +17,13 @@ pub fn build(b: *std.Build) void {
     });
     exe.root_module.addImport("stdio", stdio_dep.module("stdio"));
 
-    const run_step = b.step("run", "Run the app");
+    b.installArtifact(exe);
+
     const run_cmd = b.addRunArtifact(exe);
-    run_step.dependOn(&run_cmd.step);
     run_cmd.step.dependOn(b.getInstallStep());
+    b.step("run", "Run the app").dependOn(&run_cmd.step);
 
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const mod_tests = b.addTest(.{ .root_module = mod });
-    const run_mod_tests = b.addRunArtifact(mod_tests);
     const exe_tests = b.addTest(.{ .root_module = exe.root_module });
     const run_exe_tests = b.addRunArtifact(exe_tests);
-
-    const test_step = b.step("test", "Run tests");
-    test_step.dependOn(&run_mod_tests.step);
-    test_step.dependOn(&run_exe_tests.step);
+    b.step("test", "Run tests").dependOn(&run_exe_tests.step);
 }
